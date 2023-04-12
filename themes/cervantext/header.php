f5dc431830e695105007da33a9ca3061ad3ed641
Sebaspa0107 <sebaspa0107@gmail.com>
2023-04-11 20:42:16 -0500

    Se inicia el header

--- themes/cervantext/header.php
+++ themes/cervantext/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,40 +13,49 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'cervantext' ); ?></a>
-
+	<?php wp_body_open(); ?>
+	<div class="bg-light-pink-500">
+		<div class="container max-w-6xl mx-auto px-4">
+			<div class="py-5">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" alt="cervantext logo" class="w-60" />
+			</div>
+		</div>
+	</div>
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
+			if (is_front_page() && is_home()) :
+			?>
+				<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+			<?php
 			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
+			?>
+				<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
+			<?php
 			endif;
-			$cervantext_description = get_bloginfo( 'description', 'display' );
-			if ( $cervantext_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $cervantext_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			$cervantext_description = get_bloginfo('description', 'display');
+			if ($cervantext_description || is_customize_preview()) :
+			?>
+				<p class="site-description"><?php echo $cervantext_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+											?></p>
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'cervantext' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'cervantext'); ?></button>
 			<?php
 			wp_nav_menu(
 				array(
@@ -55,5 +65,5 @@
 			);
 			?>
 		</nav><!-- #site-navigation -->
-		
+
 	</header><!-- #masthead -->
--- themes/cervantext/tailwind.config.js
+++ themes/cervantext/tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
 	content: [ './**/*.{php,js}' ],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'light-pink': {
+					100: '#fcfaf8',
+					200: '#f9f5f2',
+					300: '#f5f0eb',
+					400: '#f2ebe5',
+					500: '#efe6de',
+					600: '#bfb8b2',
+					700: '#8f8a85',
+					800: '#605c59',
+					900: '#302e2c',
+				},
+			},
+			fontFamily: {
+				lato: [ 'Lato', 'sans-serif' ],
+			},
+		},
 	},
 	plugins: [],
 };
