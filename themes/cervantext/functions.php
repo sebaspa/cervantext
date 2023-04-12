2477f0dafcc10af918bc1b4315dce566fdf61e96
Sebaspa0107 <sebaspa0107@gmail.com>
2023-04-11 20:16:40 -0500

    Se instala tailwind

--- themes/cervantext/functions.php
+++ themes/cervantext/functions.php
@@ -139,6 +139,7 @@ add_action( 'widgets_init', 'cervantext_widgets_init' );
  */
 function cervantext_scripts() {
 	wp_enqueue_style( 'cervantext-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( 'cervantext-tw-style', get_template_directory_uri()."/dist/styles.css", array(), _S_VERSION );
 	wp_style_add_data( 'cervantext-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'cervantext-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
